7ddcbd6f9e0866fd1eda73854fd26371d1ac9023
lz100 <lzhan140@ucr.edu>
2023-10-13 18:12:47 -0700

    navbar

---
+++ app/components/ThemeSwitcher.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useState, useEffect } from 'react'
+import { useTheme } from 'next-themes'
+
+export const ThemeSwitcher = () => {
+  const [mounted, setMounted] = useState(false)
+  const { theme, setTheme } = useTheme()
+
+  // useEffect only runs on the client, so now we can safely show the UI
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return null
+  }
+
+  return (
+    <div>
+      <span>A:{theme}</span>
+    <select value={theme} onChange={e => setTheme(e.target.value)}>
+      <option value="system">System</option>
+      <option value="dark">Dark</option>
+      <option value="light">Light</option>
+    </select>
+    </div>
+
+  )
+}
+
--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,6 +1,15 @@
+"use client";
 import type { AppProps } from 'next/app'
 import './styles/globals.css'
- 
+import { NextUIProvider } from '@nextui-org/react'
+import {ThemeProvider as NextThemesProvider} from "next-themes";
+
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <NextUIProvider>
+      <NextThemesProvider attribute="class" defaultTheme="dark">
+        <Component {...pageProps} />
+      </NextThemesProvider>
+    </NextUIProvider>
+  );
 }
