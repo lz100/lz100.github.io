45f46a74d2339d5988ec9dd0d5fea31be184dec3
lz100 <lzhan140@ucr.edu>
2023-10-23 14:51:31 -0700

    homepage layout

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <>
       <NavBar/>
-      <main className="flex flex-col flex-wrap items-stretch justify-center">
+      <main className="flex flex-col flex-wrap items-stretch justify-center content-center">
         <section 
-        className='flex flex-row max-w-5xl'
+        className='flex flex-col lg:flex-row md:flex-row max-w-5xl gap-1 w-full max-w-5xl'
         style={{height: 'calc(100vh - 64px)'}}
         >
-          <div className='w-1/2' style={{backgroundColor: 'teal'}}>sdfsa sdsaf </div>
-          <div className='w-1/2' style={{backgroundColor: 'tan'}}>sdsd </div>
+          <div className='w-1/2 self-center' style={{backgroundColor: 'teal'}}>sdfsa sdsaf </div>
+          <div className='w-1/2 self-center invisible md:visible lg:visible' style={{backgroundColor: 'tan'}}>sdsd </div>
         </section>
         <section className='flex flex-row max-w-5xl flex-wrap items-stretch justify-center gap-10' style={{height: '200px'}}>
           <h1 className='text-4xl'>Hello World</h1>
